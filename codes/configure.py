a7fa0566cd2df85a4976b8d971b49db34f9aa3c3
yjcsivsuk <1473778348@qq.com>
2024-08-28 15:33:54 +0800

    在获得表达式时，忽略过小的系数仅仅是不显示在表达式中，而不是直接修改符号树的结构。应该增加一个剪枝操作。

--- codes/configure.py
+++ codes/configure.py
@@ -6,7 +6,7 @@ import torch.nn as nn
 
 # 选择实验数据集
 problem = 'heat' # 'Burgers', 'chafee-infante', 'Kdv', 'PDE_divide', 'PDE_compound', 'advection', 'ReacDiff', 'heat'
-seed = 2
+seed = 3
 device = None
 if torch.cuda.is_available():
     device = torch.device('cuda:0')
